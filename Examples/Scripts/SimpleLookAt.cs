c635e217b8f2eb81384feb3df9a7441463847e7a
Jonathan Linowes <jonathan@parkerhill.com>
2018-06-07 22:01:33 -0400

    bridge example scene with steamvr throwable and teleport

---
+++ Examples/Scripts/SimpleLookAt.cs
@@ -0,0 +1,53 @@
+﻿// Licensed under Unity Asset Store EULA Editor Extensions one license per seat
+// Copyright 2018 Parkerhill Technology Corporation www.parkerhill.com
+
+using System.Collections;
+using UnityEngine;
+
+namespace BridgeXR.Examples.Scripts
+{
+    public class SimpleLookAt : MonoBehaviour
+    {
+        private bool lookAtEnabled;
+        private Quaternion startRotation;
+
+        [SerializeField] private Transform targetTransform;
+
+        private void Awake()
+        {
+            startRotation = transform.rotation;
+        }
+
+
+        public void EnableLookAt(bool enabled)
+        {
+            lookAtEnabled = enabled;
+            StartCoroutine(lookAtEnabled ? DoLookAt() : DoResetRotation());
+        }
+
+        private IEnumerator DoResetRotation()
+        {
+            Quaternion currentRotation = transform.rotation;
+            float currentTime = Time.time;
+            float timeToComplete = .5f;
+            float fracComplete = 0;
+
+            while (lookAtEnabled == false && fracComplete < 1)
+            {
+                fracComplete = (Time.time - currentTime) / timeToComplete;
+                transform.rotation = Quaternion.Slerp(currentRotation, startRotation, fracComplete);
+                yield return 0;
+            }
+        }
+
+        private IEnumerator DoLookAt()
+        {
+            while (lookAtEnabled)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(targetTransform.position - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2);
+                yield return 0;
+            }
+        }
+    }
+}
