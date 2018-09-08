35acd0baf7d03f511ad65cdb9f3b8037e198e47f
Jonathan Linowes <jonathan@parkerhill.com>
2018-09-08 14:52:47 -0400

    restore scripts accidently committed as deleted from old exclusions code

---
+++ DeviceModules/Daydream-01/Scripts/DaydreamInputEvents.cs
@@ -0,0 +1,44 @@
+﻿using DaydreamElements.ObjectManipulation;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+// implements unity events for hover/unhover. Click to select, unclick to unselect
+// (differs from Daydream demo which is click once to select, click again to release)
+public class DaydreamInputEvents : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerUpHandler, IPointerDownHandler
+{
+    public UnityEvent OnHover;
+    public UnityEvent OnUnhover;
+    public UnityEvent OnClick;
+    public UnityEvent OnUnclick;
+    private BaseInteractiveObject interactive;
+
+    void Start()
+    {
+        interactive = GetComponent<BaseInteractiveObject>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        OnHover.Invoke();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        OnUnhover.Invoke();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnClick.Invoke();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (interactive)
+        {
+            interactive.OnPointerDown(null);
+        }
+        OnUnclick.Invoke();
+    }
+}
